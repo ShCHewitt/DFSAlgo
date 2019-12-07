6d172d37ca83b58ff7fe5171fb68bb24734c6b43
ShCHewitt <55410961+ShCHewitt@users.noreply.github.com>
2019-12-07 06:23:31 -0500

    Add files via upload

--- Fandy.java
+++ Fandy.java
@@ -75,11 +75,19 @@ public class Fandy {
         private double value;
         private double fiftyFiftyValue;
         private double consistencyStat; //fiftyFiftyValue multiplied by average fdPoints/stdDev.
+        private int fdRank;
+        private int costRank;
+        private int valueRank;
+        private int fiftyRank;
+        private int consistencyRank;
+        private double averageRank;
+        private double percentGamesStarted;
+
 
         //Constructor for the list of actual players that will be used to output to the console.
         public Player(int playerID, String name, String teamName, double averageMinutesPlayed, double averageUsageRate,
                       String position, double cost, double averagePoints, double stdDev, double value, double fiftyFiftyValue,
-                      double consistencyStat) {
+                      double consistencyStat, double percentGamesStarted) {
             this.playerID = playerID;
             this.name = name;
             this.teamName = teamName;
@@ -92,12 +100,25 @@ public class Fandy {
             this.value = value;
             this.fiftyFiftyValue = fiftyFiftyValue;
             this.consistencyStat = consistencyStat;
+            this.percentGamesStarted = percentGamesStarted;
         }
 
         public String toString() {
             String string = "Name: " + name + " Team: " + teamName + " Average Fanduel Points: " + averagePoints + " Standard Deviation: " +
                     stdDev + " Consistency Stat: " + consistencyStat + " Average Cost: " + cost;
             return string;
+        }
+    }
+
+    public class Team {
+        private String teamName;
+        private double averageFanduelPointsPerGame;
+        private double averagePlayerRanking;
+
+        public Team (String teamName, double averageFanduelPointsPerGame, double averagePlayerRanking) {
+            this.teamName = teamName;
+            this.averageFanduelPointsPerGame = averageFanduelPointsPerGame;
+            this.averagePlayerRanking = averagePlayerRanking;
         }
     }
 
@@ -128,7 +149,30 @@ public class Fandy {
     private class sortByConsistencyStat implements Comparator<Player> {
         public int compare(Player p1, Player p2) {
             return Double.compare(p2.consistencyStat, p1.consistencyStat);
-//            return (int) (p2.consistencyStat - p1.consistencyStat);
+        }
+    }
+
+    private class sortByAverageRank implements Comparator<Player> {
+        public int compare(Player p1, Player p2) {
+            return Double.compare(p1.averageRank, p2.averageRank);
+        }
+    }
+
+    private class sortByStartRate implements Comparator<Player> {
+        public int compare(Player p1, Player p2) {
+            return Double.compare(p2.percentGamesStarted, p1.percentGamesStarted);
+        }
+    }
+
+    private class sortByTeamPoints implements Comparator<Team> {
+        public int compare(Team t1, Team t2) {
+            return Double.compare(t2.averageFanduelPointsPerGame, t1.averageFanduelPointsPerGame);
+        }
+    }
+
+    private class sortByAverageTeamRank implements Comparator<Team> {
+        public int compare(Team t1, Team t2) {
+            return Double.compare(t1.averagePlayerRanking, t2.averagePlayerRanking);
         }
     }
 
@@ -183,6 +227,7 @@ public class Fandy {
             double totalMinutesPlayed = 0;
             double totalUsageRate = 0;
             int appearances = 0;
+            int gamesStarted = 0;
 
             for (int i = 0; i < map.get(player).size();i++) {
                 totalPoints += map.get(player).get(i).points;
@@ -193,12 +238,17 @@ public class Fandy {
                 totalMinutesPlayed += map.get(player).get(i).minutesPlayed;
                 totalUsageRate += map.get(player).get(i).usageRate;
                 appearances += 1;
+
+                if (map.get(player).get(i).startedGame) {
+                    gamesStarted += 1;
+                }
             }
 
             double averagePoints = totalPoints/appearances;
             double averageCost = totalCost/appearances;
             double averageMinutesPlayed = totalMinutesPlayed/appearances;
             double averageUsageRate = totalUsageRate/appearances;
+            double gamesStartedRatio = (double) gamesStarted/ (double) appearances;
 
             double dev;
             double totalDev = 0;
@@ -222,7 +272,7 @@ public class Fandy {
             //make the player.
             Player aggregatedPlayer = new Player(player, map.get(player).get(0).name, map.get(player).get(0).teamName,
                     averageMinutesPlayed, averageUsageRate, map.get(player).get(0).position, averageCost,averagePoints, stdDev,
-                    value, fifValue, consistencyValue);
+                    value, fifValue, consistencyValue, gamesStartedRatio);
 
             //put the player in the list.
             players.add(players.size(), aggregatedPlayer);
@@ -231,6 +281,7 @@ public class Fandy {
     }
 
     public ArrayList<Player> sortBy(String what, ArrayList<Player> players) {
+        //In each case, we need to instead of just printing out the toString method, actually call a display data function.
         switch (what) {
             case "fdPoints":
                 Comparator<Player> fdPoints = new sortByFanduelPoints();
@@ -252,55 +303,89 @@ public class Fandy {
                 Comparator<Player> consistency = new sortByConsistencyStat();
                 players.sort(consistency);
                 return players;
+            case "avRank":
+                Comparator<Player> avRank = new sortByAverageRank();
+                players.sort(avRank);
+                return players;
+            case "startRate":
+                Comparator<Player> startRate = new sortByStartRate();
+                players.sort(startRate);
+                return players;
         }
 
         return players;
     }
 
-    public ArrayList<Player> consoleSort(ArrayList<Player> players) {
+    //Can simplify the inputs
+    public ArrayList<Player> consoleSort(ArrayList<Player> players, Map<String, ArrayList<Player>> teams) {
+        calculateAveragePositions(players);
+
         Scanner userInput = new Scanner(System.in);
 
         String response = "";
         while (!response.equals("quit")) {
             System.out.println("What would you like to sort by? \n" +
                     "Your options are: \n" +
-                    "[Fanduel Points] Fanduel Points (High to Low)\n" +
-                    "[Average Cost] Average Cost (High to Low)\n" +
-                    "[Value] Value (High to Low)\n" +
-                    "[Fifty Value] Fifty Fifty Value (High to Low)\n" +
-                    "[Consistency Stat] Consistency Stat (High to Low)\n" +
-                    "[quit] Quit");
+                    "[1] Fanduel Points (High to Low)\n" +
+                    "[2] Average Cost (High to Low)\n" +
+                    "[3] Value (High to Low)\n" +
+                    "[4] Fifty Fifty Value (High to Low)\n" +
+                    "[5] Consistency Stat (High to Low)\n" +
+                    "[6] Average Ranking of the above metrics.\n" +
+                    "[7] Start Rate.\n" +
+                    "[8] Rank Teams by Fanduel Points per Game per Player.\n" +
+                    "[q] Quit");
 
             response = userInput.nextLine();
 
             switch (response) {
-                case "Fanduel Points":
+                case "1":
                     System.out.println("Sorting by Fanduel Points.");
                     sortBy("fdPoints", players);
-                    System.out.println(players);
+                    display(players);
+//                    System.out.println(players);
                     break;
-                case "Average Cost":
+                case "2":
                     System.out.println("Sorting by Average Cost.");
                     sortBy("cost", players);
-                    System.out.println(players);
+                    display(players);
+//                    System.out.println(players);
                     break;
-                case "Value":
+                case "3":
                     System.out.println("Sorting by Value.");
                     sortBy("value", players);
-                    System.out.println(players);
+                    display(players);
+//                    System.out.println(players);
                     break;
-                case "Fifty Value":
+                case "4":
                     System.out.println("Sorting by Fifty Fifty Value.");
                     sortBy("50val", players);
-                    System.out.println(players);
+                    display(players);
+//                    System.out.println(players);
                     break;
-                case "Consistency Stat":
+                case "5":
                     System.out.println("Sorting by Consistency Stat.");
                     sortBy("consistency", players);
-                    System.out.println(players);
+                    display(players);
+//                    System.out.println(players);
                     break;
+                case "6":
+                    System.out.println("Sorting by Average Rank");
+                    sortBy("avRank", players);
+                    display(players);
+//                    System.out.println(players);
+                    break;
+                case "7":
+                    System.out.println("Sorting by Start Rate");
+                    sortBy("startRate", players);
+                    display(players);
+                    break;
+                case "8":
+                    System.out.println("Ranking NBA teams by per Player Fanduel Points per Game.");
+                    rankTeams(teams, "teamPoints");
+                    //Doesn't output anything.
                 default:
-                    if (!response.equals("quit")) {
+                    if (!response.equals("q")) {
                         System.out.println("nothing was done because you did not enter an option.");
                     }
             }
@@ -308,18 +393,133 @@ public class Fandy {
         return players;
     }
 
+    //Calculates the average position of each sort.
+    public void calculateAveragePositions(ArrayList<Player> players) {
+        Comparator<Player> fd = new sortByFanduelPoints();
+        Comparator<Player> value = new sortByValue();
+        Comparator<Player> consistency = new sortByConsistencyStat();
+        Comparator<Player> fifT = new sortByFiftyValue();
+
+// Figure out a way to make this more optimal, it's just not right now.
+        //Not gonna sort by cost because that would not conclude anything.
+        players.sort(fd);
+        for (int i = 0; i < players.size(); i++) {
+            //Place the ranking for each player in a thing.
+            players.get(i).fdRank = players.indexOf(players.get(i));
+        }
+
+        players.sort(value);
+        for (int i = 0; i < players.size(); i++) {
+            players.get(i).valueRank = players.indexOf(players.get(i));
+        }
+
+        players.sort(consistency);
+        for (int i = 0; i < players.size(); i++) {
+            players.get(i).consistencyRank = players.indexOf(players.get(i));
+        }
+
+        players.sort(fifT);
+        for (int i = 0; i < players.size(); i++) {
+            players.get(i).fiftyRank = players.indexOf(players.get(i));
+
+            players.get(i).averageRank =  ((double) (players.get(i).fdRank + players.get(i).valueRank + players.get(i).consistencyRank
+                    + players.get(i).fiftyRank)/ (4.000));
+        }
+    }
+
+    public void display(ArrayList<Player> players) {
+        System.out.println("Name, Team\nAverageRank, Average Fanduel Points, Consistency Stat, Start Rate");
+        for (int i = 0; i < players.size(); i ++) {
+            System.out.println((i+1) + ".  " + players.get(i).name + "     " + players.get(i).teamName);
+            System.out.printf("%.2f\t", players.get(i).averageRank);
+            System.out.printf("%.2f\t", players.get(i).averagePoints);
+            System.out.printf("%.2f\t", players.get(i).consistencyStat);
+            System.out.printf("%.2f\n", players.get(i).percentGamesStarted);
+
+        }
+    }
+
+    public Map<String, ArrayList<Player>> makeTeamMap(ArrayList<Player> players) {
+        Map<String, ArrayList<Player>> teamMap = new HashMap<>();
+        Set<String> teamNames = new HashSet<>();
+
+        for (int i = 0; i < players.size(); i++) {
+            //Make the set of all of the team names.
+                teamNames.add(players.get(i).teamName);
+        }
+
+        for (String teamName: teamNames) {
+
+            ArrayList<Player> roster = new ArrayList<>();
+
+            for (int i = 0; i < players.size(); i++) {
+
+                if (players.get(i).teamName.equals(teamName)) {
+                    roster.add(roster.size(),players.get(i));
+                }
+
+            }
+
+            teamMap.put(teamName, roster);
+
+        }
+
+        return teamMap;
+    }
+
+    //Ranks the teams by the most fanduel points scored per player per game.
+    public ArrayList<Team> rankTeams (Map<String, ArrayList<Player>> teamRosters, String rankBy) {
+        ArrayList<Team> nbaTeams = new ArrayList<>();
+
+        //Loops through all of the teams in the NBA.
+        for (String team: teamRosters.keySet()) {
+            int numPlayers = 0;
+            double totalPoints = 0;
+            double totalConsistency = 0;
+            for (Player player: teamRosters.get(team)) {
+                totalPoints += player.averagePoints;
+                totalConsistency += player.averageRank;
+                numPlayers++;
+            }
+
+            Team t = new Team(team,(totalPoints/numPlayers), (totalConsistency/numPlayers));
+            nbaTeams.add(nbaTeams.size(), t);
+        }
+        if (rankBy.equals("teamPoints")) {
+            Comparator<Team> teamPoints = new sortByTeamPoints();
+
+            nbaTeams.sort(teamPoints);
+        }
+        else if (rankBy.equals("avRank")) {
+            Comparator<Team> avRank = new sortByAverageTeamRank();
+
+            nbaTeams.sort(avRank);
+        }
+
+
+        for (Team t1: nbaTeams) {
+            System.out.println(t1.teamName);
+        }
+        return nbaTeams;
+    }
+
 
 
 
     public static void main(String[] args) {
-        String filename = "data/nbaDFSdata.txt";
+        String filename = "data/data2018-2019.csv";
         Fandy shane = new Fandy();
         try {
             ArrayList<Player> players = shane.aggregateMap(shane.makePerformanceMap(filename));
 
-//            System.out.println(shane.sortBy("consistency", players));
+            Map<String, ArrayList<Player>> teamRosters = shane.makeTeamMap(players);
 
-            shane.consoleSort(players);
+            //This will eventually be just in the console sort, its only here for testing right now.
+            shane.rankTeams(teamRosters, "teamPoints");
+
+
+//            shane.consoleSort(players);
+
 
         } catch (Exception e) {
             System.out.println(e);
